9fa8c3882f5fa92ae624ae736ff4748bf14cfe8f
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-12 10:23:48 +0530

    Make the home page ui responsive and add few products

---
+++ home.js
@@ -0,0 +1,59 @@
+let productsHTML = ``;
+products.forEach((product) => {
+    productsHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <!-- images -->
+                <img class="product-image" src="${product.image}">
+                <!-- stars -->
+                <div class="product-rating-container">
+                    <img class="product-rating-stars"
+                        src="images/ratings/rating-${product.rating.stars * 10}.png">
+                    <span class="product-rating-count link-primary">
+                        ${product.rating.count}
+                    </span>
+                </div>
+            </div>
+            <div class="lower-container">
+                <div class="name-qty">
+                    <!-- product name -->
+                    <div class="name-tag">Seasons®</div>
+                    <div class="product-quantity-container">
+                        <select class="select-qty">
+                            <option selected value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="product-name limit-text-to-1-lines">
+                    ${product.name}
+                </div>
+                <!-- price & qty -->
+                <div class="price-qty-container">
+                    <div class="product-price">
+                        ₹${product.price}
+                    </div>
+                    <button class="add-to-cart-button js-add-to-cart">
+                        Add to cart
+                    </button>
+                </div>
+            </div>			
+        </div>
+    `;
+});
+
+document.querySelector('.products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('clicked');
+    });
+});
---
+++ products.js
@@ -0,0 +1,192 @@
+const products = [{
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/product-images/Batman-tshirt.webp",
+    rating: {
+        stars: 4.5,
+        count: 87
+    },
+    name: "Men's Black The Batman Printed T-shirt",
+    price: 579,
+    keywords: [
+        "tshirt",
+        "Batman",
+        "Men",
+        "Black"
+      ]
+}, {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/product-images/shoes-spider.gif",
+    rating: {
+        stars: 5,
+        count: 101
+    },
+    name: "Men's Black & Red Spider-Verse Color Block Low-Top Sneakers",
+    price: 1999,
+    keywords: [
+        "Sneakers",
+        "Spider",
+        "Man",
+        "shoes"
+      ]
+}, {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/product-images/men-s-brown-oversized-joggers-646510-1736509076-1.webp",
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    name: "Men's Beige Oversized Joggers",
+    price: 999,
+    keywords: [
+        "Joggers",
+        "Oversized",
+        "Men",
+        "Brown"
+    ]
+}, {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/product-images/womens-mens-white-invertebros-graphic-printed-oversized-t-shirt-649878-1736314871-1.webp",
+    rating: {
+        stars: 5,
+        count: 2197
+      },
+    name: "Women's White Invertebros Graphic Printed Oversized T-shirt",
+    price: 799,
+    keywords: [
+        "Womens",
+        "White",
+        "Oversized",
+        "t-shirt"
+    ]
+}, {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/product-images/women-s-black-wide-leg-pants-654848-1736336943-1.webp",
+    rating: {
+        stars: 4,
+        count: 37
+    },
+    name: "Women's Black Wide Leg Pants",
+    price: 1299,
+    keywords: [
+        "Womens",
+        "Black",
+        "Wide",
+        "Pants"
+    ]
+}, {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "images/product-images/men-s-white-introvert-typography-oversized-t-shirt-611250-1741259217-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 175
+    },
+    name: "Men's White Introvert Typography Oversized T-shirt",
+    price: 599,
+    keywords: [
+      "Men",
+      "White",
+      "t-shirt",
+      "Oversized"
+    ]
+}, {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/product-images/women-s-black-white-graphic-printed-short-top-648493-1736314971-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 317
+    },
+    name: "Women's Black & White Polka Printed Short Top",
+    price: 899,
+    keywords: [
+        "Black",
+        "Women",
+        "Polka",
+        "Short",
+        "Top"
+    ]
+}, {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: "images/product-images/women-stripe-slim-fit-top-1-620175-1702643595-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 144
+    },
+    name: "Women's White & Pink Striped Slim Fit Short Top",
+    price: 699,
+    keywords: [
+        "Top",
+        "Short",
+        "Women",
+        "Striped",
+        "Pink",
+        "White"
+    ]
+}, {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: "images/product-images/women-s-beige-all-over-printed-short-top-647146-1728995951-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 305
+    },
+    name: "Women's Beige & Black All Over Printed Short Top",
+    price: 799,
+    keywords: [
+        "Top",
+        "Short",
+        "Women",
+        "Striped",
+        "Black",
+        "Beige"
+    ]
+}, {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: "images/product-images/men-s-black-the-panda-way-graphic-printed-oversized-t-shirt-580633-1738673393-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 235
+    },
+    name: "Men's Black The Panda Way Graphic Printed Oversized T-shirt",
+    price: 599,
+    keywords: [
+        "Men",
+        "Black",
+        "Panda",
+        "Oversized",
+        "t-shirt",
+        "printed"
+    ]
+}, {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: "images/product-images/men-s-black-spider-punk-graphic-printed-zipper-sweatshirt-651188-1736428385-1.gif",
+    rating: {
+        stars: 5,
+        count: 790
+    },
+    name: "Men's Black Spider Punk Graphic Printed Zipper Sweatshirt",
+    price: 799,
+    keywords: [
+        "Men",
+        "Black",
+        "Spider",
+        "Sweatshirt",
+        "t-shirt",
+        "Zipper",
+        "Punk"
+    ]
+}, {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/product-images/women-s-navy-blue-graphic-printed-oversized-hoodies-641002-1733719798-1.webp",
+    rating: {
+        stars: 4.5,
+        count: 30
+    },
+    name: "Women's Navy Blue Graphic Printed Oversized ",
+    price: 899,
+    keywords: [
+        "Women",
+        "Navy Blue",
+        "Graphic",
+        "Oversized",
+        "Hoodies"
+    ]
+}];
