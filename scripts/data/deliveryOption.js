c17205b77576e5873a4136efdc15aa346a1502cf
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-18 10:55:18 +0530

    Skipping wweekend days

--- scripts/data/deliveryOption.js
+++ scripts/data/deliveryOption.js
@@ -24,9 +24,18 @@ export function getDeliveryOption(deliveryOptionId) {
     return deliveryOption;
 }
 
+function isWeekend(date) {
+    return date.day() === 6 || date.day() === 0; //6 = Saturday, 0 = Sunday
+}
+
 export function calculateDeliveryDate(deliveryOption) {
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    let remainingDays = deliveryOption.deliveryDays;
+    let deliveryDate = dayjs();
+    
+    while (remainingDays > 0) {
+        deliveryDate = deliveryDate.add(1, 'day');
+        if(!isWeekend(deliveryDate)) remainingDays--;
+    }
     const dateString = deliveryDate.format('dddd, MMMM D');
 
     return dateString;
